8b3b659f00dee08976d6ad607e79ceae0142a042
jgalaber <jgalaberdev@gmail.com>
2025-08-29 22:41:12 +0200

    ci: add git hooks

--- scripts/setup-git-hooks.sh
+++ scripts/setup-git-hooks.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
+chmod +x .githooks/pre-commit
+chmod +x .githooks/pre-push
+
 git config core.hooksPath .githooks
 echo "✅ Git hooks enabled (core.hooksPath = .githooks)"
